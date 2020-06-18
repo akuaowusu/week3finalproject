3eb486169b424f8f12074258f0f878b0c0d4b55f
Akua Owusu <akowusu@davidson.edu>
2020-06-18 16:56:55 -0400

    js ideas

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-javascript document
+//maybe have 4 classes: a,b,c,d
+//we store which class the user picks for each question in an array
+//give a cactus based off which class they picked the most
+    //so we calculate which letter occurs the most in the array once there's 5 letters
+//if we go for 5 questions what happens if they pick 2 a's 2 b's and 1 c?
+$('a').on('click', function() {
+  console.log(a);
+});
+$('b').on('click', function() {
+  console.log(b);
+});
+$('c').on('click', function() {
+  console.log(c);
+});
+$('d').on('click', function() {
+  console.log(d);
+});
