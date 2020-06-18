a461425dc0897acc8f405f7e6f2f55aac93f0735
Akua Owusu <akowusu@davidson.edu>
2020-06-18 17:25:02 -0400

    more js ideas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 //give a cactus based off which class they picked the most
     //so we calculate which letter occurs the most in the array once there's 5 letters
 //if we go for 5 questions what happens if they pick 2 a's 2 b's and 1 c?
+//how will the results show up? in an alert? link to a new page?
+
 $('a').on('click', function() {
   console.log(a);
 });
