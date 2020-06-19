0a831a9a46aacf338b24c69afdfcde5c374179de
Ramez Attia <ramezattia@lclark.edu>
2020-06-18 19:52:52 -0700

    finalVersion

--- index.js
+++ index.js
@@ -5,15 +5,72 @@
 //if we go for 5 questions what happens if they pick 2 a's 2 b's and 1 c?
 //how will the results show up? in an alert? link to a new page?
 
-$('a').on('click', function() {
-  console.log(a);
+let questions = {};
+let letters = [];
+
+$('.a').on('click', function(event) {
+  console.log('a');
+  let id = event.currentTarget.closest('.cactuscontainer').id;
+  console.log(event);
+  questions[id]="a";
+  console.log(letters)
 });
-$('b').on('click', function() {
-  console.log(b);
+
+$('.b').on('click', function() {
+  console.log('b');
+  let id = event.currentTarget.closest('.cactuscontainer').id;
+  questions[id]="b";
+  console.log(letters)
 });
-$('c').on('click', function() {
-  console.log(c);
+
+$('.c').on('click', function() {
+  console.log('c');
+  let id = event.currentTarget.closest('.cactuscontainer').id;
+  questions[id]="c";
+  console.log(letters)
 });
-$('d').on('click', function() {
-  console.log(d);
+
+$('.d').on('click', function() {
+  console.log('d');
+  let id = event.currentTarget.closest('.cactuscontainer').id;
+  questions[id]="d";
+  console.log(id);
+  console.log(id, questions);
 });
+
+$('.done').on('click', function() {
+  quiz();
+});
+
+function quiz() {
+  let questionIDs = Object.keys(questions)
+  let frequency = {};
+  console.log("1",questionIDs);
+  for(var i = 0; i < questionIDs.length; ++i) {
+      let key = questionIDs [i];
+      console.log(key);
+      console.log(questions);
+      let values = questions[key];
+      console.log(values);
+      if (frequency[values]) {
+        frequency[values]++;
+        console.log("this", frequency[values])
+      }
+      else {
+        frequency[values] = 0;
+        frequency[values]++;
+        console.log("else", frequency[values])
+      }
+   }
+    console.log(frequency)
+    let keys = Object.keys(frequency);
+    let container = 0;
+    for (var i=0; i < keys.length; ++i) {
+      let key = keys[i];
+      let values = frequency[key];
+      if (values > container) {
+        container = values;
+        console.log(container);
+      }
+    }
+}
